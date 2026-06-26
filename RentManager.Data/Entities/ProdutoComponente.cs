a957f4925464b08e3b5202aed524742c9ec08b6e
Murilo <Murilo@DESKTOP-M6R1LM4>
2026-06-25 21:30:22 -0300

    feat: adiciona suporte a produtos compostos (jogos)

---
+++ RentManager.Data/Entities/ProdutoComponente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentManager.Data.Entities
+{
+    public class ProdutoComponente
+    {
+        public int Id { get; set; }
+
+        public int ProdutoPaiId { get; set; }
+        public Produto ProdutoPai { get; set; }
+
+        public int ProdutoFilhoId { get; set; }
+        public Produto ProdutoFilho { get; set; }
+
+        // Quantas unidades do filho são consumidas por 1 unidade do pai
+        public int QuantidadePorUnidade { get; set; }
+    }
+}
