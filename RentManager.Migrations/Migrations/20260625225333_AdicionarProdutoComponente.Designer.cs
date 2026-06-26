72d4cc35d5633bcf9a2bc50d85231e50cd94a891
Murilo <Murilo@DESKTOP-M6R1LM4>
2026-06-25 21:30:22 -0300

    feat: adiciona suporte a produtos compostos (jogos)

--- RentManager.Migrations/Migrations/20260625225333_AdicionarProdutoComponente.Designer.cs
+++ RentManager.Migrations/Migrations/20260625225333_AdicionarProdutoComponente.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentManager.Data;
 
@@ -11,9 +12,11 @@ using RentManager.Data;
 namespace RentManager.Migrations.Migrations
 {
     [DbContext(typeof(RentManagerDataBase))]
-    partial class RentManagerDataBaseModelSnapshot : ModelSnapshot
+    [Migration("20260625225333_AdicionarProdutoComponente")]
+    partial class AdicionarProdutoComponente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RentManager.Migrations/Migrations/20260625225333_AdicionarProdutoComponente.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentManager.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarProdutoComponente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProdutosComponentes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProdutoPaiId = table.Column<int>(type: "int", nullable: false),
+                    ProdutoFilhoId = table.Column<int>(type: "int", nullable: false),
+                    QuantidadePorUnidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProdutosComponentes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProdutosComponentes_Produtos_ProdutoFilhoId",
+                        column: x => x.ProdutoFilhoId,
+                        principalTable: "Produtos",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ProdutosComponentes_Produtos_ProdutoPaiId",
+                        column: x => x.ProdutoPaiId,
+                        principalTable: "Produtos",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProdutosComponentes_ProdutoFilhoId",
+                table: "ProdutosComponentes",
+                column: "ProdutoFilhoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProdutosComponentes_ProdutoPaiId_ProdutoFilhoId",
+                table: "ProdutosComponentes",
+                columns: new[] { "ProdutoPaiId", "ProdutoFilhoId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProdutosComponentes");
+        }
+    }
+}
